cc1e59c3f5943cd4aed297c53013f6cde7757f2b
rahul <rahul@ritika-r>
2020-08-17 02:55:16 +0530

    latest code commit and push - 16-AUG-2020

--- MavenTestNgTest/src/test/java/com/qa/tests/Testqa.java
+++ MavenTestNgTest/src/test/java/com/qa/tests/Testqa.java
@@ -1,10 +1,15 @@
 package com.qa.tests;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -20,20 +25,66 @@ import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class Testqa 
 {
-	public static void testMethodr() 
-	{
-		
-	}
 	WebDriver driver;
 	@Test
-	public void openFlipkart() 
+	public void ixigoTicketBookingTest() throws Exception 
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\rahul\\Downloads\\softwares\\Selenium-Java\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.flipkart.com/");
+		driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
+		driver.get("https://www.ixigo.com/");
 		String pageTitle = driver.getTitle();
-		Assert.assertTrue(pageTitle.contains("Online Shopping"));
+		//2.	Validate the page
+		Assert.assertTrue(pageTitle.contains("ixigo - Flights, Train Reservation, Hotels, Air Tickets, Bus Booking"));
+		
+		//3.	Enter From – Pune , To – Hyderabad
+		WebElement SourceCity = driver.findElement(By.xpath("//div[@class='orgn u-ib u-v-align-bottom u-text-left']//input[@placeholder='Enter city or airport']"));
+		Thread.sleep(2000);
+		SourceCity.click();
+		SourceCity.sendKeys(Keys.CONTROL , "a");
+		SourceCity.sendKeys(Keys.BACK_SPACE);
+		Thread.sleep(2000);
+		SourceCity.sendKeys("Pune");
+		Thread.sleep(2000);
+		SourceCity.sendKeys(Keys.ENTER);
+		
+		WebElement DestinationCity = driver.findElement(By.xpath("//div[@class='dstn u-ib u-v-align-bottom u-text-left']//input[@placeholder='Enter city or airport']"));
+		Thread.sleep(2000);
+		DestinationCity.click();
+		DestinationCity.sendKeys(Keys.CONTROL , "a");
+		DestinationCity.sendKeys(Keys.BACK_SPACE);
+		Thread.sleep(2000);
+		DestinationCity.sendKeys("Hyderabad");
+		Thread.sleep(2000);
+		DestinationCity.sendKeys(Keys.ENTER);
+		
+		//Departure – 17 Sep 2020 / 17 Sep, Thu
+		WebElement departureDate = driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/table[1]/tbody[1]/tr[3]/td[5]/div[1]"));
+		departureDate.click();
+		
+		//Return – 24 Oct 2020
+		driver.findElement(By.xpath("//input[@placeholder='Return']")).click();
+		WebElement returnDate = driver.findElement(By.xpath("/html[1]/body[1]/div[5]/div[2]/div[2]/table[1]/tbody[1]/tr[4]/td[7]/div[1]"));
+		returnDate.click();
+		
+		//Travelers - 2
+		driver.findElement(By.xpath("//div[@class='passanger-class-input u-pos-rel']//input[@class='c-input u-v-align-middle']")).click();
+		String Adult_numberOfPassengers = "2";
+		driver.findElement(By.xpath("//div[@class='passanger-class-input u-pos-rel']//div[1]//div[2]//span["+Adult_numberOfPassengers+"]")).click();
+		
+		//4.	Click on Search, Validate the result page
+		driver.findElement(By.xpath("//button[contains(text(),'Search')]")).click();
+		Thread.sleep(20000);
+		
+		String currentPageTitle02 = driver.getTitle();
+		Assert.assertTrue(currentPageTitle02.contains("Pune - Hyderabad, Economy Flights, roundtrip, 17 Sep - 24 Oct"));
+		
+		//5.	Validate filter option for Stops, departure and Airlines – Select Non-Stop in Stops filter option
+		
+		
+		
+		
 	}
 
 	ExtentReports extent;
@@ -54,22 +105,9 @@ public class Testqa
 		
 		extent.setSystemInfo("Author", "Rahul Ranjan");
 		extent.setSystemInfo("environment", "QA");
-		extent.setSystemInfo("pattern", "self");
+		extent.setSystemInfo("pattern", "self-designed");
 	}
-	@Test
-	public void ClickSignIn() 
-	{
-		test = extent.createTest("CeateTestReports", "ClickSignIn");
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\rahul\\Downloads\\softwares\\Selenium-Java\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://boardgamegeek.com/");
-		driver.findElement(By.xpath("//*[@id=\"global-header-outer\"]/header/nav/div/div[2]/div/div[1]/ul/li[7]")).click();
-		driver.findElement(By.id("inputUsername")).sendKeys("test123");
-		driver.findElement(By.id("inputPassword")).sendKeys("testPassword123");
-		driver.findElement(By.xpath("/html/body/div[1]/div/div/form/div[3]/button")).click();
-		System.out.println(System.getProperty("testSuite"));
-	}
+	
 	@AfterMethod
 	public void getResult(ITestResult result)
 	{
