cd6ae4591a920182fe5152d3dd0e814c87486532
rahul <rahul@ritika-r>
2020-08-18 16:22:31 +0530

    18082020-QACheckins-ixigo-flightBookings

--- MavenTestNgTest/src/test/java/com/qa/tests/ExecutableClass_Testqa.java
+++ MavenTestNgTest/src/test/java/com/qa/tests/ExecutableClass_Testqa.java
@@ -23,12 +23,21 @@ public class ExecutableClass_Testqa
 	@Test
 	public void ixigoTicketBookingTest() throws Exception 
 	{
-		//create page objects using PageFactory
-		IxigoPages_Homepage IxigoPages_Homepage = PageFactory.initElements(driver, IxigoPages_Homepage.class);
-		IxigoPages_Homepage.ixigoSearch_page("Pune", "Hyderabad", "2");
+		try 
+		{
+			//create page objects using PageFactory
+			IxigoPages_Homepage IxigoPages_Homepage = PageFactory.initElements(driver, IxigoPages_Homepage.class);
+			IxigoPages_Homepage.ixigoSearch_page("Pune", "Hyderabad", "2");
+			
+			//create page objects using PageFactory
+			IxigoPages_FlightBooking IxigoPages_FlightBooking = PageFactory.initElements(driver, IxigoPages_FlightBooking.class);
+			IxigoPages_FlightBooking.ixigoBooking_page();
+		}catch(Exception ex)
+		{
+			System.out.println(ex.getStackTrace());
+			System.out.println(ex.getMessage());
+		}
 		
-		IxigoPages_FlightBooking IxigoPages_FlightBooking = PageFactory.initElements(driver, IxigoPages_FlightBooking.class);
-		IxigoPages_FlightBooking.ixigoBooking_page();
 	}
 	
 	@AfterTest
